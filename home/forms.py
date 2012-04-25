e9a5a4d18b8833ea0dc430d6ea1d07319d73c1fe
Jason Novinger <jnovinger@gmail.com>
2012-04-25 16:56:14 -0500

    Added payment_type to form and changed it to a radio button widget.

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
-from models import Donation
+from django.forms.fields import ChoiceField
+from django.forms.widgets import RadioSelect
+from models import Donation, PMT_CHOICES
 
 class DonationForm(ModelForm):
+    payment_type = ChoiceField(widget=RadioSelect, choices=PMT_CHOICES)
     class Meta:
         model = Donation
         exclude = ['date', 'received', 'cleared', ]

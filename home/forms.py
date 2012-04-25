7ef1f6ed787620593792e29e607dfba034a48506
Jason Novinger <jnovinger@gmail.com>
2012-04-25 17:03:13 -0500

    Set the zip field max_length to 5.

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 from django.forms.fields import ChoiceField
 from django.forms.widgets import RadioSelect
 from models import Donation, PMT_CHOICES
 
 class DonationForm(ModelForm):
     payment_type = ChoiceField(widget=RadioSelect, choices=PMT_CHOICES)
+    zip = CharField(max_length=5)
     class Meta:
         model = Donation
         exclude = ['date', 'received', 'cleared', ]

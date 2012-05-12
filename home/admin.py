0c17ada3943dacbd413485f89a2f8a82e60a6904
Jason Novinger <jnovinger@gmail.com>
2012-05-12 00:54:14 -0500

    No reason not to have sections in the admin.

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Post, Donation
+from models import Post, Donation, Section
 
 admin.site.register(Post)
 admin.site.register(Donation)
+admin.site.register(Section)

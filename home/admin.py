93e083f1952bb89e23b918e96e60703da7fce6ba
Jason Novinger <jnovinger@gmail.com>
2012-05-18 22:38:51 -0500

    Customize Donation list display.

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from models import Post, Donation, Section
 
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'first_name', 'last_name', 'received', 'cleared', 'payment_type', 'get_sections')
+
 admin.site.register(Post)
-admin.site.register(Donation)
+admin.site.register(Donation, DonationAdmin)
 admin.site.register(Section)
